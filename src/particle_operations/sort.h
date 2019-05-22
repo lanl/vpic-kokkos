c3b598984d45c26358cd65e07e621d9b13b2594b
Robert Bird <bird@lanl.gov>
2019-05-22 11:03:05 -0600

    port sort over to policy based sort

--- src/particle_operations/sort.h
+++ src/particle_operations/sort.h
@@ -1,41 +1,34 @@
 #ifndef PARTICLE_SORT_POLICY_H
 #define PARTICLE_SORT_POLICY_H
 
-// TODO: document
+#include <Kokkos_Sort.hpp>
+#include <Kokkos_DualView.hpp>
+#include "../vpic/kokkos_helpers.h"
+
+/**
+ * @brief Simple bin sort using Kokkos inbuilt sort
+ */
 struct DefaultSort {
     static void sort(
             k_particles_t particles,
             const int32_t np,
-            species_t* sp
+            const int32_t num_bins
     )
     {
         // Try grab the index's for a permute key
-        int pi = particle_var::pi; // TODO: can you really not pass an enum in??
-        auto keys = Kokkos::subview(sp->k_p_d, Kokkos::ALL, pi);
-        using key_type = decltype(keys);
+        int pi = particle_var::pi; // FIXME: can you really not pass an enum in??
+        auto keys = Kokkos::subview(particles, Kokkos::ALL, pi);
 
-        // TODO: we can tighten the bounds on this
-        int max = accumulator_array->na;
+        // TODO: we can tighten the bounds on this to avoid ghosts
+
+        using key_type = decltype(keys);
         using Comparator = Kokkos::BinOp1D<key_type>;
-        Comparator comp(max, 0, max);
+        Comparator comp(num_bins, 0, num_bins);
 
         int sort_within_bins = 0;
-        Kokkos::BinSort<key_type, Comparator> bin_sort(keys, 0, sp->np, comp, sort_within_bins );
+        Kokkos::BinSort<key_type, Comparator> bin_sort(keys, 0, np, comp, sort_within_bins );
         bin_sort.create_permute_vector();
-        bin_sort.sort(sp->k_p_d);
-    }
-
-};
-
-struct SortCompress {
-    static void compress(
-            k_particles_t particles,
-            k_particle_movers_t particle_movers,
-            const int32_t nm,
-            const int32_t np,
-            species_t* sp
-            )
-    {
+        bin_sort.sort(particles);
     }
 
 };
