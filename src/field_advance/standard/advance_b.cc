16a62911b3c0b06f0ea04d6192d3e3cf93d85b5c
Nigel Phillip Tan <ntan@lanl.gov>
2019-06-12 14:14:45 -0600

    Switched to MDRangePolicy for loops in advance_b

--- src/field_advance/standard/advance_b.cc
+++ src/field_advance/standard/advance_b.cc
@@ -37,102 +37,47 @@ void advance_b_kokkos(k_field_t k_field, const size_t nx, const size_t ny, const
   #define UPDATE_CBY() f0_cby -= ( pz*( fz_ex-f0_ex ) - px*( fx_ez-f0_ez ) );
   #define UPDATE_CBZ() f0_cbz -= ( px*( fx_ey-f0_ey ) - py*( fy_ex-f0_ex ) );
 
-  Kokkos::parallel_for("advance b", KOKKOS_TEAM_POLICY_DEVICE
-      (nz, Kokkos::AUTO),
-      KOKKOS_LAMBDA
-      (const KOKKOS_TEAM_POLICY_DEVICE::member_type &team_member)
-  {
-    const size_t z = team_member.league_rank() + 1;
-
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, ny), [=] (size_t yi) {
-      const size_t y = yi + 1;
-
-      Kokkos::parallel_for(Kokkos::ThreadVectorRange(team_member, nx), [=] (size_t xi) {
-        const size_t x = xi + 1;
-        //printf("%d %d %d %d\n", x ,y,z, ny);
-        size_t f0_index = VOXEL(x,  y,  z,   nx,ny,nz);
-        size_t fx_index = VOXEL(x+1,y,  z,   nx,ny,nz);
-        size_t fy_index = VOXEL(x,  y+1,z,   nx,ny,nz);
-        size_t fz_index = VOXEL(x,  y,  z+1, nx,ny,nz);
-
-        UPDATE_CBX();
-        UPDATE_CBY();
-        UPDATE_CBZ();
-
-        });
-    });
-  });
-/*
-    Kokkos::parallel_for(KOKKOS_TEAM_POLICY_DEVICE
-        (nv, Kokkos::AUTO),
-        KOKKOS_LAMBDA
-        (const KOKKOS_TEAM_POLICY_DEVICE::member_type &team_member) {
-      const size_t v = team_member.league_rank();
-
-      size_t f0_index = VOXEL(1,  1,  1,   nx,ny,nz) + v;
-      size_t fx_index = VOXEL(2,  1,  1,   nx,ny,nz) + v;
-      size_t fy_index = VOXEL(1,  2,  1,   nx,ny,nz) + v;
-      size_t fz_index = VOXEL(1,  1,  2,   nx,ny,nz) + v;
-
-      UPDATE_CBX();
-      UPDATE_CBY();
-      UPDATE_CBZ();
-  });
-*/
   // Do the bulk of the magnetic fields in the pipelines.  The host
   // handles stragglers.
   // While the pipelines are busy, do surface fields
-  //
 
-  // Do left over bx
-  Kokkos::parallel_for("advance b bx", KOKKOS_TEAM_POLICY_DEVICE
-      (nz, Kokkos::AUTO),
-      KOKKOS_LAMBDA
-      (const KOKKOS_TEAM_POLICY_DEVICE::member_type &team_member) {
-    const size_t z = team_member.league_rank() + 1;
-
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, ny), [=] (const size_t yi) {
-      const size_t y = yi + 1;
-
-      const size_t f0_index = VOXEL(nx+1,y,  z,  nx,ny,nz);
-      const size_t fy_index = VOXEL(nx+1,y+1,z,  nx,ny,nz);
-      const size_t fz_index = VOXEL(nx+1,y,  z+1,nx,ny,nz);
-      UPDATE_CBX();
+    Kokkos::MDRangePolicy<Kokkos::Rank<3>> xyz_policy({1,1,1},{nz+1,ny+1,nx+1});
+    Kokkos::parallel_for("advance_b main chunk", xyz_policy, KOKKOS_LAMBDA(const int z, const int y, const int x) {
+        size_t f0_index = VOXEL(x,   y,   z,    nx,ny,nz);
+        size_t fx_index = VOXEL(x+1, y,   z,    nx,ny,nz);
+        size_t fy_index = VOXEL(x,   y+1, z,    nx,ny,nz);
+        size_t fz_index = VOXEL(x,   y,   z+1,  nx,ny,nz);
+        UPDATE_CBX();
+        UPDATE_CBY();
+        UPDATE_CBZ();
     });
-  });
+  
+  // Do left over bx
+    Kokkos::MDRangePolicy<Kokkos::Rank<2>> zy_policy({1,1},{nz+1,ny+1});
+    Kokkos::parallel_for("advance_b::bx", zy_policy, KOKKOS_LAMBDA(const int z, const int y) {
+        const size_t f0_index = VOXEL(nx+1,y,  z,  nx,ny,nz);
+        const size_t fy_index = VOXEL(nx+1,y+1,z,  nx,ny,nz);
+        const size_t fz_index = VOXEL(nx+1,y,  z+1,nx,ny,nz);
+        UPDATE_CBX();
+    });
 
   // Do left over by
-  Kokkos::parallel_for("advance b by", KOKKOS_TEAM_POLICY_DEVICE
-      (nz, Kokkos::AUTO),
-      KOKKOS_LAMBDA
-      (const KOKKOS_TEAM_POLICY_DEVICE::member_type &team_member) {
-    const size_t z = team_member.league_rank() + 1;
-
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, nx), [=] (const size_t i) {
-      const size_t f0_index = VOXEL(1,ny+1,z,  nx,ny,nz) + i;
-      const size_t fx_index = VOXEL(2,ny+1,z,  nx,ny,nz) + i;
-      const size_t fz_index = VOXEL(1,ny+1,z+1,nx,ny,nz) + i;
-
-      UPDATE_CBY();
-
+    Kokkos::MDRangePolicy<Kokkos::Rank<2>> zx_policy({1,1},{nz+1,nx+1});
+    Kokkos::parallel_for("advance_b::by", zx_policy, KOKKOS_LAMBDA(const int z, const int x) {
+        const size_t f0_index = VOXEL(1,ny+1, z,  nx,ny,nz) + (x-1);
+        const size_t fx_index = VOXEL(2,ny+1, z,  nx,ny,nz) + (x-1);
+        const size_t fz_index = VOXEL(1,ny+1, z+1,nx,ny,nz) + (x-1);
+        UPDATE_CBY();
     });
-  });
 
   // Do left over bz
-  Kokkos::parallel_for("advance b bz", KOKKOS_TEAM_POLICY_DEVICE
-      (ny, Kokkos::AUTO),
-      KOKKOS_LAMBDA
-      (const KOKKOS_TEAM_POLICY_DEVICE::member_type &team_member) {
-    const size_t y = team_member.league_rank() + 1;
-
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, nx), [=] (const size_t i) {
-      const size_t f0_index = VOXEL(1,y,  nz+1,nx,ny,nz) + i;
-      const size_t fx_index = VOXEL(2,y,  nz+1,nx,ny,nz) + i;
-      const size_t fy_index = VOXEL(1,y+1,nz+1,nx,ny,nz) + i;
-
-      UPDATE_CBZ();
+    Kokkos::MDRangePolicy<Kokkos::Rank<2>> yx_policy({1,1},{ny+1,nx+1});
+    Kokkos::parallel_for("advance_b::bz", yx_policy, KOKKOS_LAMBDA(const int y, const int x) {
+        const size_t f0_index = VOXEL(1,y,   nz+1,  nx,ny,nz) + (x-1);
+        const size_t fx_index = VOXEL(2,y,   nz+1,  nx,ny,nz) + (x-1);
+        const size_t fy_index = VOXEL(1,y+1, nz+1,  nx,ny,nz) + (x-1);
+        UPDATE_CBZ();
     });
-  });
 
 }
 
