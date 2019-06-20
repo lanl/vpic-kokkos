1bb5ae143406d91bb6a0fdfa4e9a3ecb47262330
Robert Bird <bird@lanl.gov>
2019-06-20 08:58:44 -0600

    change sort to also respect reinterpet_cast

--- src/particle_operations/sort.h
+++ src/particle_operations/sort.h
@@ -5,6 +5,49 @@
 #include <Kokkos_DualView.hpp>
 #include "../vpic/kokkos_helpers.h"
 
+// We need this because we use the bits in particle_var::pi as ints but store
+// them as floats. It should really go away and we should move pi into it's own
+// struct
+template<class KeyViewType> struct Casted_BinOp1D {
+  int max_bins_;
+  double mul_;
+  typename KeyViewType::const_value_type range_;
+  typename KeyViewType::const_value_type min_;
+
+  Casted_BinOp1D():max_bins_(0),mul_(0.0),
+            range_(typename KeyViewType::const_value_type()),
+            min_(typename KeyViewType::const_value_type()) {}
+
+  //Construct BinOp with number of bins, minimum value and maxuimum value
+  Casted_BinOp1D(int max_bins__, typename KeyViewType::const_value_type min,
+                               typename KeyViewType::const_value_type max )
+     :max_bins_(max_bins__+1),mul_(1.0*max_bins__/(max-min)),range_(max-min),min_(min) {}
+
+  //Determine bin index from key value
+  template<class ViewType>
+  KOKKOS_INLINE_FUNCTION
+  int bin(ViewType& keys, const int& i) const {
+    return int(mul_*(keys(i)-min_));
+  }
+
+  //Return maximum bin index + 1
+  KOKKOS_INLINE_FUNCTION
+  int max_bins() const {
+    return max_bins_;
+  }
+
+  //Compare to keys within a bin if true new_val will be put before old_val
+  template<class ViewType, typename iType1, typename iType2>
+  KOKKOS_INLINE_FUNCTION
+  bool operator()(ViewType& keys, iType1& i1, iType2& i2) const {
+      int a = keys(i1);
+      int b = keys(i2);
+      int _a = reinterpret_cast<int&>(a);
+      int _b = reinterpret_cast<int&>(b);
+      return a < b;
+  }
+};
+
 /**
  * @brief Simple bin sort using Kokkos inbuilt sort
  */
@@ -22,7 +65,7 @@ struct DefaultSort {
         // TODO: we can tighten the bounds on this to avoid ghosts
 
         using key_type = decltype(keys);
-        using Comparator = Kokkos::BinOp1D<key_type>;
+        using Comparator = Casted_BinOp1D<key_type>;
         Comparator comp(num_bins, 0, num_bins);
 
         int sort_within_bins = 0;
