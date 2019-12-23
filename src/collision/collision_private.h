b10b9bcc32e11c20daa434fdbeeb2dfdc584bead
Robert Bird <bird@lanl.gov>
2019-12-23 10:31:59 -0700

    copy over all collision files from devel

--- src/collision/collision_private.h
+++ src/collision/collision_private.h
@@ -20,6 +20,8 @@ struct collision_op {
   collision_op_t * next;
 };
 
+BEGIN_C_DECLS
+
 void
 checkpt_collision_op_internal( const collision_op_t * cop );
 
@@ -37,10 +39,13 @@ new_collision_op_internal( void * params,
 void
 delete_collision_op_internal( collision_op_t * cop );
 
+END_C_DECLS
+
 ///////////////////////////////////////////////////////////////////////////////
 // Langevin pipeline interface
 
-typedef struct langevin_pipeline_args {
+typedef struct langevin_pipeline_args
+{
   MEM_PTR( particle_t, 128 ) p;
   MEM_PTR( rng_t,      128 ) rng[ MAX_PIPELINE ];
   float decay; 
@@ -49,6 +54,6 @@ typedef struct langevin_pipeline_args {
   PAD_STRUCT( (1+MAX_PIPELINE)*SIZEOF_MEM_PTR+2*sizeof(float)+sizeof(int) )
 } langevin_pipeline_args_t;
 
-PROTOTYPE_PIPELINE( langevin, langevin_pipeline_args_t );
+// PROTOTYPE_PIPELINE( langevin, langevin_pipeline_args_t );
 
 #endif /* _collision_h_ */
