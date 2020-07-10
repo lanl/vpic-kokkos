a9241f5e889441f358f24557eb750cd9b9df84e4
W. Joe Meese <meese_wj@lanl.gov>
2020-07-10 16:57:41 -0500

    Moved some things around and started zigzag again
    Since it looks like the particle weight factors between the
    Villasenor and Umeda algorithms are almost the same (up to that
    extra 1/12 term in the Villasenor paper), the idea now is to move
    the particles manually along the zigzag, and then have the
    current accumulate over this new trajectory.
    
    The end result is a lot of if statements that should be, in
    principle, possible to cut down once we know the idea works. The
    code runs and spits out the correct 1d zig currents, but fails on
    the 1d zag currents. I haven't tried higher dimensions. I also
    added more comments to the older code to help future readers
    understand move_p_kokkos(...) without as much difficulty.
    
    Two things that will need to be addressed. First, even in the old
    code, it looks the the particle displacement is unnecessarily
    multiplied by a factor of 2 when axis == 3. This I do not quite
    get yet, and so I don't know how important it is. Second, it is
    not clear how to shoot a particle through a corner instead of only
    along one cube face as it is done now. If this cannot be easily
    done, it's not clear that the zigzag algorithm will actually be
    any different than what's currently implemented in the code.

--- src/species_advance/accumulate_j.hpp
+++ src/species_advance/accumulate_j.hpp
@@ -131,4 +131,66 @@
     p_dy += s_dispy+s_dispy;
     p_dz += s_dispz+s_dispz;
 
+    // If an end streak, return success (should be ~50% of the time)
+    //printf("axis %d x %e y %e z %e disp x %e y %e z %e\n", axis, p_dx, p_dy, p_dz, s_dispx, s_dispy, s_dispz);
 
+    if( axis==3 ) 
+    {
+        printf("\n*****************************\nParticle %d is done moving at p_dx, p_dy, p_dz = %e, %e, %e\nIt is supposed to stop at x2, y2, z2 = %e, %e, %e\n****************************\n",
+                pi, p_dx, p_dy, p_dz, joe_midx + joe_dispx, joe_midy + joe_dispy, joe_midz + joe_dispz);
+        break;
+    }
+
+    // Determine if the particle crossed into a local cell or if it
+    // hit a boundary and convert the coordinate system accordingly.
+    // Note: Crossing into a local cell should happen ~50% of the
+    // time; hitting a boundary is usually a rare event.  Note: the
+    // entry / exit coordinate for the particle is guaranteed to be
+    // +/-1 _exactly_ for the particle.
+
+    v0 = s_dir[axis];
+    k_particles(pi, particle_var::dx + axis) = v0; // Avoid roundoff fiascos--put the particle
+                           // _exactly_ on the boundary.
+    face = axis; if( v0>0 ) face += 3;
+
+    // TODO: clean this fixed index to an enum
+    //neighbor = g->neighbor[ 6*ii + face ];
+    neighbor = d_neighbor( 6*ii + face );
+
+    // TODO: these two if statements used to be marked UNLIKELY,
+    // but that intrinsic doesn't work on GPU.
+    // for performance portability, maybe specialize UNLIKELY
+    // for CUDA mode and put it back
+
+
+    if( neighbor==reflect_particles ) {
+      // Hit a reflecting boundary condition.  Reflect the particle
+      // momentum and remaining displacement and keep moving the
+      // particle.
+      printf("\n*************************\nParticle %d was reflected.\n************************\n", pi);
+      k_particles(pi, particle_var::ux + axis) = -k_particles(pi, particle_var::ux + axis);
+
+      // TODO: make this safer
+      //(&(pm->dispx))[axis] = -(&(pm->dispx))[axis];
+      //k_local_particle_movers(0, particle_mover_var::dispx + axis) = -k_local_particle_movers(0, particle_mover_var::dispx + axis);
+      // TODO: replace this, it's horrible
+      (&(pm->dispx))[axis] = -(&(pm->dispx))[axis];
+
+
+      continue;
+    }
+
+    if( neighbor<rangel || neighbor>rangeh ) {
+      // Cannot handle the boundary condition here.  Save the updated
+      // particle position, face it hit and update the remaining
+      // displacement in the particle mover.
+      pii = 8*pii + face;
+      return 1; // Return "mover still in use"
+      }
+
+    // Crossed into a normal voxel.  Update the voxel index, convert the
+    // particle coordinate system and keep moving the particle.
+
+    pii = neighbor - rangel;
+    /**/                         // Note: neighbor - rangel < 2^31 / 6
+    k_particles(pi, particle_var::dx + axis) = -v0;      // Convert coordinate system
--- src/species_advance/accumulate_j_zigzag.hpp
+++ src/species_advance/accumulate_j_zigzag.hpp
@@ -12,6 +12,13 @@
     THIS FILE IS THE ZIGZAG ONE!!!!!!!!!!!!!!!!!!!
 
    ****************************************************************/
+    
+    // In this approach, we keep the current accumulation the same,
+    // and move the particles along the zigzag path. The position 
+    // of the zig of the zigzag will be performed only if the 
+    // particle leaves the cell. If the particle does not leave the
+    // cell, then the zag is performed.
+
     int ii = pii;
     s_midx = p_dx;
     s_midy = p_dy;
@@ -26,12 +33,21 @@
 
     //printf("\nParticle %d: disp x %e y %e z %e", pi, s_dispx, s_dispy, s_dispz);
 
+    // Compute the direction that the particle moves.
+    // This value is the also the boundary of the cell 
+    // the particle will intercept.
     s_dir[0] = (s_dispx>0) ? 1 : -1;
     s_dir[1] = (s_dispy>0) ? 1 : -1;
     s_dir[2] = (s_dispz>0) ? 1 : -1;
 
     // Compute the twice the fractional distance to each potential
-    // streak/cell face intersection.
+    // streak/cell face intersection. This number is the amount of
+    // for-loop steps that would need to be taken to reach the 
+    // boundary of the cell along each axis. A maximum of 2 for-
+    // loop steps are allowed within a single time-step though, so
+    // if the value is greater than 2 in a particle direction, then
+    // it does not leave the cell in that direction in this
+    // function call.
     v0 = (s_dispx==0) ? 3.4e38f : (s_dir[0]-s_midx)/s_dispx;
     v1 = (s_dispy==0) ? 3.4e38f : (s_dir[1]-s_midy)/s_dispy;
     v2 = (s_dispz==0) ? 3.4e38f : (s_dir[2]-s_midz)/s_dispz;
@@ -46,6 +62,8 @@
     if(v0<v3) v3=v0, axis=0;
     if(v1<v3) v3=v1, axis=1;
     if(v2<v3) v3=v2, axis=2;
+    // Multiply v3 by 1/2 because the particle first moves to the 
+    // midpoint if axis != 3, or it stops if axis == 3.
     v3 *= 0.5;
     
     printf("\nParticle %d: axis, v0, v1, v2, v3 = %d, %e, %e, %e, %e",
@@ -54,24 +72,66 @@
             pi, s_midx, s_midy, s_midz);
     printf("\nParticle %d: s_dispx, s_dispy, s_dispz = %e, %e, %e\n",
             pi, s_dispx, s_dispy, s_dispz);
+    
+    // Store the old values of s_mid and s_disp before I do crazy
+    // things.
+    float old_midx = s_midx, old_midy = s_midy, old_midz = s_midz;
+    float old_dispx = s_dispx, old_dispy = s_dispy, old_dispz = s_dispz;
 
-    float joe_midx = s_midx, joe_midy = s_midy, joe_midz = s_midz;
-    float joe_dispx = s_dispx, joe_dispy = s_dispy, joe_dispz = s_dispz;
+    // Umeda algorithm: assume axis == 3 and set xr, yr, and zr
+    // to be the end of the midpoint of the zag (so the final
+    // destination of the particle).
+    // TODO: Is it necessary to half the displacement when
+    // axis == 3?
+    float xr = s_midx + 0.5 * s_dispx, yr = s_midy + 0.5 * s_dispy, zr = s_midz + 0.5 * s_dispz;
+   
+    // If the particle crosses the x-boundary change xr
+    // to the boundary it hits.
+    if ( v0 < 2. )
+    {
+        xr = s_dir[0];
+        s_dispx *= v0;
+    }
+    // If the particle crosses the y-boundary change yr
+    // to the boundary it hits.
+    if ( v1 < 2. )
+    {
+        yr = s_dir[1];
+        s_dispy *= v1;
+    }
+    // If the particle crosses the z-boundary change zr
+    // to the boundary it hits.
+    if ( v2 < 2. )
+    {
+        zr = s_dir[2];
+        s_dispz *= v2;
+    } 
+    // With xr, yr, and zr known, we can treat them as the final 
+    // location on either the zig or the zag. Now we just need 
+    // the new midpoint along this new linear zig or zag.
+    s_midx = 0.5 * ( s_midx + xr );
+    s_midy = 0.5 * ( s_midy + yr );
+    s_midz = 0.5 * ( s_midz + zr );
 
-    // Compute the midpoint and the normalized displacement of the streak
+    // Change the displacement to the midpoint along the zig
+    // or zag.
+    s_dispx *= 0.5, s_dispy *= 0.5, s_dispz *= 0.5;
+
+    // Compute the midpoint and the normalized displacement of the 
+    // streak. By scaling the displacments by v3, if axis == 3, then
+    // nothing is done (v3 == 1.0 at this point), but if axis != 3,
+    // then s_disp is set to s_dir - s_mid along the appropriate
+    // axis, while the other displacements are scaled to keep the
+    // particle on the same linear trajectory.
+    /*
     s_dispx *= v3;
     s_dispy *= v3;
     s_dispz *= v3;
     s_midx += s_dispx;
     s_midy += s_dispy;
     s_midz += s_dispz;
+    */
     
-    printf("\nAfter rescaling...");
-    printf("\nParticle %d: s_midx, s_midy, s_midz = %e, %e, %e",
-            pi, s_midx, s_midy, s_midz);
-    printf("\nParticle %d: s_dispx, s_dispy, s_dispz = %e, %e, %e\n",
-            pi, s_dispx, s_dispy, s_dispz);
-
     // Accumulate the streak.  Note: accumulator values are 4 times
     // the total physical charge that passed through the appropriate
     // current quadrant in a time-step
@@ -79,192 +139,49 @@
 
     //a = (float *)(&d_accumulators[ci]);
 
-
-    // Obtain the cell indices for the particles.
-    /*
-    float i1 = floor( 0.5 * ( 1. + joe_midx ) ), i2 = floor( 0.5 * ( 1. + joe_midx + joe_dispx ) );
-    float j1 = floor( 0.5 * ( 1. + joe_midy ) ), j2 = floor( 0.5 * ( 1. + joe_midy + joe_dispy ) );
-    float k1 = floor( 0.5 * ( 1. + joe_midz ) ), k2 = floor( 0.5 * ( 1. + joe_midz + joe_dispz ) );
-    */
-    
-    /* 
-    // Destroy the elegance of Umeda's algorithm for funsies.
-    // By default i1,j1,k1 should all be the left-side of the 
-    // cell, i.e. should be 0 unless the particle is exactly
-    // on the boundary.
-    float i1 = 0.f, j1 = 0.f, k1 = 0.f;
-    if ( joe_midx == 1.f ) i1 = 1.f;
-    if ( joe_midy == 1.f ) j1 = 1.f;
-    if ( joe_midz == 1.f ) k1 = 1.f;
-
-    // Assume that the particle stays in-cell so that its 
-    // Umeda indices are identical. If the particle leaves the cell,
-    // increment the index appropriately. Right now, we assume that
-    // a particle moving at light speed can travel one cell in one
-    // time step.
-    float i2 = i1, j2 = j1, k2 = k1;
-    if ( joe_midx + joe_dispx >= 1.f ) i2 = 1.f;
-    if ( joe_midx + joe_dispx == 3.f ) i2 = 2.f;    // This should never happen.
-    if ( joe_midx + joe_dispx <= -1.f ) i2 = -1.f;
-    if ( joe_midx + joe_dispx == -3.f ) i2 = -2.f;  // This should never happen.
-    
-    if ( joe_midy + joe_dispy >= 1.f ) j2 = 1.f;
-    if ( joe_midy + joe_dispy == 3.f ) j2 = 2.f;    // This should never happen.
-    if ( joe_midy + joe_dispy <= -1.f ) j2 = -1.f;
-    if ( joe_midy + joe_dispy == -3.f ) j2 = -2.f;  // This should never happen.
-    
-    if ( joe_midz + joe_dispz >= 1.f ) k2 = 1.f;
-    if ( joe_midz + joe_dispz == 3.f ) k2 = 2.f;    // This should never happen.
-    if ( joe_midz + joe_dispz <= -1.f ) k2 = -1.f;
-    if ( joe_midz + joe_dispz == -3.f ) k2 = -2.f;  // This should never happen.
-    */
-
-#define cell2umeda_pt(val) 0.5 * ( 1. + val )       // Convert a point from [-1,1] to [0,1]
-#define cell2umeda_diff(val) 0.5 * val              // Convert a difference from [-1,1] to [0,1]
-#define umeda2cell_pt(val) -1. + 2. * val           // Convert a point from [0,1] to [-1,1]
-#define umeda2cell_diff(val) 2. * val               // Convert a difference from [0,1] to [-1,1]
+#   define accumulate_j(X,Y,Z)                                        \
+    v4  = q*s_disp##X;    /* v2 = q ux                            */  \
+    v1  = v4*s_mid##Y;    /* v1 = q ux dy                         */  \
+    v0  = v4-v1;          /* v0 = q ux (1-dy)                     */  \
+    v1 += v4;             /* v1 = q ux (1+dy)                     */  \
+    v4  = 1+s_mid##Z;     /* v4 = 1+dz                            */  \
+    v2  = v0*v4;          /* v2 = q ux (1-dy)(1+dz)               */  \
+    v3  = v1*v4;          /* v3 = q ux (1+dy)(1+dz)               */  \
+    v4  = 1-s_mid##Z;     /* v4 = 1-dz                            */  \
+    v0 *= v4;             /* v0 = q ux (1-dy)(1-dz)               */  \
+    v1 *= v4;             /* v1 = q ux (1+dy)(1-dz)               */  \
+    v0 += v5;             /* v0 = q ux [ (1-dy)(1-dz) + uy*uz/3 ] */  \
+    v1 -= v5;             /* v1 = q ux [ (1+dy)(1-dz) - uy*uz/3 ] */  \
+    v2 -= v5;             /* v2 = q ux [ (1-dy)(1+dz) - uy*uz/3 ] */  \
+    v3 += v5;             /* v3 = q ux [ (1+dy)(1+dz) + uy*uz/3 ] */  \
+    //Kokkos::atomic_add(&a[0], v0); \
+    //Kokkos::atomic_add(&a[1], v1); \
+    //Kokkos::atomic_add(&a[2], v2); \
+    //Kokkos::atomic_add(&a[3], v3);
    
-    joe_midx = cell2umeda_pt(joe_midx), joe_midy = cell2umeda_pt(joe_midy), joe_midz = cell2umeda_pt(joe_midz);
-    joe_dispx = cell2umeda_diff(joe_dispx), joe_dispy = cell2umeda_diff(joe_dispy), joe_dispz = cell2umeda_diff(joe_dispz);
-     
-    // Obtain the cell indices for the particles.
-    float i1 = floor(joe_midx), i2 = floor(joe_midx + joe_dispx);
-    float j1 = floor(joe_midy), j2 = floor(joe_midy + joe_dispy);
-    float k1 = floor(joe_midz), k2 = floor(joe_midz + joe_dispz);
-
-    printf("\nParticle %d: i1, j1, k1 = %d, %d, %d",
-            pi, (int)i1, (int)j1, (int)k1);
-    printf("\nParticle %d: i2, j2, k2 = %d, %d, %d",
-            pi, (int)i2, (int)j2, (int)k2);
-
-    // Obtain the midpoints for the trajectory over one timestep
-    float xmid = joe_midx + 0.5 * joe_dispx;
-    float ymid = joe_midy + 0.5 * joe_dispy;
-    float zmid = joe_midz + 0.5 * joe_dispz;
-
-    // Obtain the reference points for the particles
-    // Here the constant 1.f comes from cell coordinates.
-    float xr = fmin( fmin(i1, i2) + 1.f, fmax( fmax(i1, i2), xmid ) );
-    float yr = fmin( fmin(j1, j2) + 1.f, fmax( fmax(j1, j2), ymid ) );
-    float zr = fmin( fmin(k1, k2) + 1.f, fmax( fmax(k1, k2), zmid ) );
-
-    int lval = ( axis == 3 ? 2 : 1 );
-    if ( axis == 3 )
-    {
-        xr = joe_midx, yr = joe_midy, zr = joe_midz;
-    }
-
-    printf("\nParticle %d: xmid, ymid, zmid = %e, %e, %e\n",
-            pi, xmid, ymid, zmid);
-    printf("\nParticle %d: xr, yr, zr = %e, %e, %e\n",
-            pi, xr, yr, zr);
-
-    // Get the fluxes in the Umeda coordinates
-    float Fx1 = q * ( xr - joe_midx  );
-    float Fy1 = q * ( yr - joe_midy  );
-    float Fz1 = q * ( zr - joe_midz  );
-    
-    float Fx2 = q * p_ux - Fx1;
-    float Fy2 = q * p_uy - Fy1;
-    float Fz2 = q * p_uz - Fz1;
-    
-    // Finally, get the weights in the Umeda coordinates
-    float Wx1 = 0.5 * ( joe_midx + xr ) - i1;
-    float Wx2 = 0.5 * ( joe_midx + joe_dispx + xr ) - i2;
-    
-    float Wy1 = 0.5 * ( joe_midy + yr ) - j1;
-    float Wy2 = 0.5 * ( joe_midy + joe_dispy + yr ) - j2;
-    
-    float Wz1 = 0.5 * ( joe_midz + zr ) - k1;
-    float Wz2 = 0.5 * ( joe_midz + joe_dispz + zr ) - k2;
-    
-    joe_midx = umeda2cell_pt(joe_midx), joe_midy = umeda2cell_pt(joe_midy), joe_midz = umeda2cell_pt(joe_midz);
-    joe_dispx = umeda2cell_diff(joe_dispx), joe_dispy = umeda2cell_diff(joe_dispy), joe_dispz = umeda2cell_diff(joe_dispz);
-    
-#undef cell2umeda_pt
-#undef cell2umeda_diff
-#undef umeda2cell_pt
-#undef umeda2cell_diff
-
-    // This function is defined for the four edges of Jx. Then
-    // cyclically permute X,Y,Z to get Jy and Jz weights.
-    // I believe all units are already normalized by cell volume...
-    // TODO: Verify that l = 1,2 is not actually necessary when
-    // particles move.
-#   define accumulate_j_zigzag(X,Y,Z,l)                                                      \
-    v0 = F##X##l * (1 - W##Y##l) * (1 - W##Z##l); /*   v0 = Fux * (1 - Wuy) * (1 - Wuz)   */ \
-    printf("\nUmeda v0 = %e", v0);                                                           \
-    v1 = F##X##l * W##Y##l * (1 - W##Z##l);       /*   v1 = Fux * Wuy * (1 - Wuz)         */ \
-    v2 = F##X##l * (1 - W##Y##l) * W##Z##l;       /*   v2 = Fux * (1 - Wuy) * Wuz         */ \
-    v3 = F##X##l * W##Y##l * W##Z##l;             /*   v2 = Fux * Wuy * Wuz               */ \
-    /*   Now convert from the Umeda coordinates back into VPIC cell cooridinates          */ \
-    /*   These equations guarantee that the total flux summed around the edges            */ \
-    /*   are Fx = 2Fux as they should be (both the fluxes and the weights                 */ \
-    /*   transform as differences between coordinate systems.)                            */ \
-    printf("\nFux, Wuy, Wuz = %e, %e, %e", F##X##l, W##Y##l, W##Z##l);                       \
-    v0 = 8 * v0; /*- 4 * F##X##l * ( 1.5 - W##Y##l - W##Z##l );*/                            \
-    printf("\nVPIC v0 = %e\n", v0);                                                          \
-    v1 = 8 * v1; /*- 4 * F##X##l * W##Y##l;      */                                          \
-    v2 = 8 * v2; /* - 4 * F##X##l * W##Z##l;  */                                             \
-    v3 = 8 * v3;
-
-    if ( lval == 1 )
-    {   
-        accumulate_j_zigzag(x,y,z,1);
-    }
-    if ( lval == 2 )
-    {
-        accumulate_j_zigzag(x,y,z,2); 
-    }
-    printf("\nParticle %d depositing (x,y,z,l) v0, v1, v2, v4 = %e, %e, %e, %e, %d", pi, v0, v1, v2, v3, lval);
+    accumulate_j(x,y,z);
+    printf("\nParticle %d depositing (x,y,z) v0, v1, v2, v4 = %e, %e, %e, %e", pi, v0, v1, v2, v3);
     k_accumulators_scatter_access(ii, accumulator_var::jx, 0) += v0;
     k_accumulators_scatter_access(ii, accumulator_var::jx, 1) += v1;
     k_accumulators_scatter_access(ii, accumulator_var::jx, 2) += v2;
     k_accumulators_scatter_access(ii, accumulator_var::jx, 3) += v3;
 
-
-    if ( lval == 1 )
-    {
-        accumulate_j_zigzag(y,z,x,1);
-    }
-    if ( lval == 2 )
-    {
-        accumulate_j_zigzag(y,z,x,2); 
-    }
-    printf("\nParticle %d depositing (y,z,x,l) v0, v1, v2, v4 = %e, %e, %e, %e, %d", pi, v0, v1, v2, v3, lval);
+    accumulate_j(y,z,x);
+    printf("\nParticle %d depositing (y,z,x) v0, v1, v2, v4 = %e, %e, %e, %e", pi, v0, v1, v2, v3);
     k_accumulators_scatter_access(ii, accumulator_var::jy, 0) += v0;
     k_accumulators_scatter_access(ii, accumulator_var::jy, 1) += v1;
     k_accumulators_scatter_access(ii, accumulator_var::jy, 2) += v2;
     k_accumulators_scatter_access(ii, accumulator_var::jy, 3) += v3;
 
-    if ( lval == 1 )
-    {
-        accumulate_j_zigzag(z,x,y,1);
-    }
-    if ( lval == 2 )
-    {
-        accumulate_j_zigzag(z,x,y,2);  
-    }
-    printf("\nParticle %d depositing (z,x,y,l) v0, v1, v2, v4 = %e, %e, %e, %e, %d\n\n", pi, v0, v1, v2, v3, lval);
+    accumulate_j(z,x,y);
+    printf("\nParticle %d depositing (z,x,y) v0, v1, v2, v4 = %e, %e, %e, %e\n\n", pi, v0, v1, v2, v3);
     k_accumulators_scatter_access(ii, accumulator_var::jz, 0) += v0;
     k_accumulators_scatter_access(ii, accumulator_var::jz, 1) += v1;
     k_accumulators_scatter_access(ii, accumulator_var::jz, 2) += v2;
     k_accumulators_scatter_access(ii, accumulator_var::jz, 3) += v3;
 
-#   undef accumulate_j_zigzag
-    
-    /*
-    // Compute the remaining particle displacment
-    pm->dispx = joe_dispx - ( xr - joe_midx );
-    pm->dispy = joe_dispy - ( yr - joe_midy );
-    pm->dispz = joe_dispz - ( zr - joe_midz );
+#   undef accumulate_j
 
-    //printf("pre axis %d x %e y %e z %e disp x %e y %e z %e\n", axis, p_dx, p_dy, p_dz, s_dispx, s_dispy, s_dispz);
-    // Compute the new particle offset
-    p_dx = xr;
-    p_dy = yr;
-    p_dz = zr;
-    */
 
     // Compute the remaining particle displacment
     pm->dispx -= s_dispx;
@@ -273,9 +190,73 @@
 
     //printf("pre axis %d x %e y %e z %e disp x %e y %e z %e\n", axis, p_dx, p_dy, p_dz, s_dispx, s_dispy, s_dispz);
     // Compute the new particle offset
+    // TODO: In the case that axis == 3, this seems to overcompensate for the change in position...
     p_dx += s_dispx+s_dispx;
     p_dy += s_dispy+s_dispy;
     p_dz += s_dispz+s_dispz;
 
+    // If an end streak, return success (should be ~50% of the time)
+    //printf("axis %d x %e y %e z %e disp x %e y %e z %e\n", axis, p_dx, p_dy, p_dz, s_dispx, s_dispy, s_dispz);
 
+    if( axis==3 ) 
+    {
+        printf("\n*****************************\nParticle %d is done moving at p_dx, p_dy, p_dz = %e, %e, %e\nIt is supposed to stop at x2, y2, z2 = %e, %e, %e\n****************************\n",
+                pi, p_dx, p_dy, p_dz, xr, yr, zr);
+        break;
+    }
+
+    // Determine if the particle crossed into a local cell or if it
+    // hit a boundary and convert the coordinate system accordingly.
+    // Note: Crossing into a local cell should happen ~50% of the
+    // time; hitting a boundary is usually a rare event.  Note: the
+    // entry / exit coordinate for the particle is guaranteed to be
+    // +/-1 _exactly_ for the particle.
+
+    // TODO: Change this to allow for corner crossing.
+    v0 = s_dir[axis];
+    k_particles(pi, particle_var::dx + axis) = v0; // Avoid roundoff fiascos--put the particle
+                           // _exactly_ on the boundary.
+    face = axis; if( v0>0 ) face += 3;
+
+    // TODO: clean this fixed index to an enum
+    //neighbor = g->neighbor[ 6*ii + face ];
+    neighbor = d_neighbor( 6*ii + face );
+
+    // TODO: these two if statements used to be marked UNLIKELY,
+    // but that intrinsic doesn't work on GPU.
+    // for performance portability, maybe specialize UNLIKELY
+    // for CUDA mode and put it back
+
+
+    if( neighbor==reflect_particles ) {
+      // Hit a reflecting boundary condition.  Reflect the particle
+      // momentum and remaining displacement and keep moving the
+      // particle.
+      printf("\n*************************\nParticle %d was reflected.\n************************\n", pi);
+      k_particles(pi, particle_var::ux + axis) = -k_particles(pi, particle_var::ux + axis);
+
+      // TODO: make this safer
+      //(&(pm->dispx))[axis] = -(&(pm->dispx))[axis];
+      //k_local_particle_movers(0, particle_mover_var::dispx + axis) = -k_local_particle_movers(0, particle_mover_var::dispx + axis);
+      // TODO: replace this, it's horrible
+      (&(pm->dispx))[axis] = -(&(pm->dispx))[axis];
+
+
+      continue;
+    }
+
+    if( neighbor<rangel || neighbor>rangeh ) {
+      // Cannot handle the boundary condition here.  Save the updated
+      // particle position, face it hit and update the remaining
+      // displacement in the particle mover.
+      pii = 8*pii + face;
+      return 1; // Return "mover still in use"
+      }
+
+    // Crossed into a normal voxel.  Update the voxel index, convert the
+    // particle coordinate system and keep moving the particle.
+
+    pii = neighbor - rangel;
+    /**/                         // Note: neighbor - rangel < 2^31 / 6
+    k_particles(pi, particle_var::dx + axis) = -v0;      // Convert coordinate system
 
