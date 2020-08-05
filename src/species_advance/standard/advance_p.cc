e4f2623cfb5f28b375c02ac1a4ac5d8efe3d97d4
W. Joe Meese <meese_wj@lanl.gov>
2020-08-05 18:42:23 -0500

    Trying this

--- src/species_advance/standard/advance_p.cc
+++ src/species_advance/standard/advance_p.cc
@@ -232,8 +232,8 @@ sp_[id]->
     //if(  !(v3<=one &&  v4<=one &&  v5<=one && -v3<=one && -v4<=one && -v5<=one) ) {
     // Check if the particle is going to leave the cell on either boundary. 
     // A single axis is sufficient to engage zigzags.
-    if ( (v3 > one || v3 < minus_one) || (v4 > one || v4 < minus_one) || (v5 > one || v5 < minus_one) ) {
-    //if(  v3 > one || v4 > one || v5 > one || -v3 < one || -v4 < one || -v5 < one ) {
+    //if ( (v3 > one || v3 < minus_one) || (v4 > one || v4 < minus_one) || (v5 > one || v5 < minus_one) ) {
+    if(  v3 > one || v4 > one || v5 > one || -v3 > one || -v4 > one || -v5 > one ) {
 
         // zagger = false;
 
