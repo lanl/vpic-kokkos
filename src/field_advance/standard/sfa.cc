dacdf5f9fc4ceadfd0882f39c8babd5d15991af6
Scott V. Luedtke <SLuedtke@lanl.gov>
2020-08-26 17:47:01 -0600

    Field buffers are instantiated once, not every timestep.  They are not properly deallocated, and you will get errors on exit.

--- src/field_advance/standard/sfa.cc
+++ src/field_advance/standard/sfa.cc
@@ -242,6 +242,15 @@ new_standard_field_array( grid_t           * RESTRICT g,
     fa->kernel->clean_div_e_kokkos= vacuum_clean_div_e_kokkos;
     fa->kernel->energy_f_kokkos   = vacuum_energy_f_kokkos;
   }
+  int nx = g->nx;
+  int ny = g->ny;
+  int nz = g->nz;
+
+  int xyz_sz = 2*ny*(nz+1) + 2*nz*(ny+1) + ny*nz;
+  int yzx_sz = 2*nz*(nx+1) + 2*nx*(nz+1) + nz*nx;
+  int zxy_sz = 2*nx*(ny+1) + 2*ny*(nx+1) + nx*ny;
+  static field_buffers_t buffs = field_buffers(xyz_sz, yzx_sz, zxy_sz);
+  fa->fb = &buffs;
 
   REGISTER_OBJECT( fa, checkpt_standard_field_array,
                        restore_standard_field_array, NULL );
--- src/field_advance/standard/vacuum_advance_e.cc
+++ src/field_advance/standard/vacuum_advance_e.cc
@@ -586,19 +586,13 @@ vacuum_advance_e_kokkos( field_array_t * RESTRICT fa,
   const float pz_muy = ((nz>1) ? (1+damp)*g->cvac*g->dt*g->rdz : 0)*m->rmuy; 
   const float pz_mux = ((nz>1) ? (1+damp)*g->cvac*g->dt*g->rdz : 0)*m->rmux; 
   const float cj     = g->dt/g->eps0;                                        
-    
-    // Field buffers for GPU - GPU communication
-    const int xyz_sz = 1 + ny*(nz+1) + nz*(ny+1);
-    const int yzx_sz = 1 + nz*(nx+1) + nx*(nz+1);
-    const int zxy_sz = 1 + nx*(ny+1) + ny*(nx+1);
-    field_buffers_t f_buffers = field_buffers_t(xyz_sz, yzx_sz, zxy_sz);
 
   /***************************************************************************
    * Begin tangential B ghost setup
    ***************************************************************************/
   
 //    k_begin_remote_ghost_tang_b( fa, fa->g );
-    kokkos_begin_remote_ghost_tang_b(fa, fa->g, f_buffers);
+    kokkos_begin_remote_ghost_tang_b(fa, fa->g, *fa->fb);
 
     k_local_ghost_tang_b( fa, fa->g );
 
@@ -609,7 +603,7 @@ vacuum_advance_e_kokkos( field_array_t * RESTRICT fa,
    ***************************************************************************/
 
 //    k_end_remote_ghost_tang_b( fa, fa->g );
-    kokkos_end_remote_ghost_tang_b(fa, fa->g, f_buffers);
+    kokkos_end_remote_ghost_tang_b(fa, fa->g, *fa->fb);
 
   /***************************************************************************
    * Update exterior fields
