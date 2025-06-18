e70753cee0d72ee6d922765b18239f0341fe0b64
Ari Le <arile@lanl.gov>
2025-06-18 16:26:56 -0600

    Add electrostatic solver

--- src/field_advance/standard/hyb_advance_pe.cc
+++ src/field_advance/standard/hyb_advance_pe.cc
@@ -5,10 +5,7 @@
 #include <iostream>
 
 #define F(ind,v) k_field(f##ind##_index, field_var::v)
-  
-#define  ROTEX()  ( py*( F(y,ez) - F(my,ez) ) - pz*( F(z,ey) - F(mz,ey) ) )	
-#define  ROTEY()  ( pz*( F(z,ex) - F(mz,ex) ) - px*( F(x,ez) - F(mx,ez) ) )	
-#define  ROTEZ()  ( px*( F(x,ey) - F(mx,ey) ) - py*( F(y,ex) - F(my,ex) ) )	
+	
 
 #define DIVUEP() \
   ( px*(F(x,ux)*F(x,pe) - F(mx,ux)*F(mx,pe) )\
@@ -44,38 +41,23 @@
   float dpedt  =  gamma * DIVUEP() + (1.0-gamma) * UEGRADP() + kappa * DIVQE();
 
 #define UPDATE_B(delt)				\
-  F(0,cbx) = F(0,ox) - delt*ROTEX();		\
-  F(0,cby) = F(0,oy) - delt*ROTEY();		\
-  F(0,cbz) = F(0,oz) - delt*ROTEZ();		\
-  F(0,pe)  = (F(0,rhof) > denmin) ? F(0,oe) - delt*dpedt : F(0,te0)*F(0,rhof);
+   F(0,pe)  = (F(0,rhof) > denmin) ? F(0,oe) - delt*dpedt : F(0,te0)*F(0,rhof);
 
 #define UPDATE1()		\
   UPDATE_B(dt2);		\
-  F(0,tx) = ROTEX();		\
-  F(0,ty) = ROTEY();		\
-  F(0,tz) = ROTEZ();		\
-  F(0,te) = dpedt
+   F(0,te) = dpedt
 
 #define UPDATE2()		\
   UPDATE_B(dt2);		\
-  F(0,tx) += two*ROTEX();	\
-  F(0,ty) += two*ROTEY();	\
-  F(0,tz) += two*ROTEZ();	\
-  F(0,te) += two*dpedt;
+    F(0,te) += two*dpedt;
 
 #define UPDATE3()		\
   UPDATE_B(dt);			\
-  F(0,tx) += two*ROTEX();	\
-  F(0,ty) += two*ROTEY();	\
-  F(0,tz) += two*ROTEZ();	\
-  F(0,te) += two*dpedt;
+    F(0,te) += two*dpedt;
   
 #define UPDATE4()		\
   UPDATE_B(dt6);		\
-  F(0,cbx) -= dt6*F(0,tx);	\
-  F(0,cby) -= dt6*F(0,ty);	\
-  F(0,cbz) -= dt6*F(0,tz);	\
-  F(0,pe)  -= dt6*F(0,te);	\
+    F(0,pe)  -= dt6*F(0,te);	\
   F(0,pe)  += rV*two_thirds*F(0,se);\
   F(0,se)   = 0;                 \
   F(0,pe) = (F(0,rhof)>denmin) ? F(0,pe) : F(0,te0)*F(0,rhof);\
@@ -108,39 +90,36 @@ hyb_advance_bpe(field_array_t * RESTRICT fa,
 //Store initial B
   Kokkos::parallel_for("store b_old", Kokkos::RangePolicy<>(0,nv),
 		       KOKKOS_LAMBDA(const int v) {
-			 k_field(v, field_var::ox) = k_field(v, field_var::cbx);
-			 k_field(v, field_var::oy) = k_field(v, field_var::cby);
-			 k_field(v, field_var::oz) = k_field(v, field_var::cbz);
- 			 k_field(v, field_var::oe) = k_field(v, field_var::pe);
+			k_field(v, field_var::oe) = k_field(v, field_var::pe);
 		       });
 
   // ----------------------------------------------------------
   // 0: Setup and smooth ion moments
   // ----------------------------------------------------------
     
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments");
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments");
   //Only smooth on the first subcycle
   if (isub==0) {
     
     //smooth moments
-    Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments::Exchange_JF");
+    Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments::Exchange_JF");
     k_begin_remote_ghost_hyb_jf(fa, fa->g, *(fa->fb) );
     k_end_remote_ghost_hyb_jf  (fa, fa->g, *(fa->fb) );
     Kokkos::Profiling::popRegion();
-    Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments::Apply_Local_Ghost_JF");
+    Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments::Apply_Local_Ghost_JF");
     k_hyb_local_ghost_jf  (fa, fa->g);
     Kokkos::Profiling::popRegion();
 
     int ism = g->nsm;
     while(ism>0) {
-      Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments::Smooth_Moments");
+      Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments::Smooth_Moments");
       hyb_smooth_moments( fa );
       Kokkos::Profiling::popRegion();
-      Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments::Exchange_JF");
+      Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments::Exchange_JF");
       k_begin_remote_ghost_hyb_jf(fa, fa->g, *(fa->fb) );
       k_end_remote_ghost_hyb_jf  (fa, fa->g, *(fa->fb) );
       Kokkos::Profiling::popRegion();
-      Kokkos::Profiling::pushRegion("HybyridAdvanceB::Smooth_Ion_Moments::Apply_Local_Ghost_JF");
+      Kokkos::Profiling::pushRegion("HybridAdvancePe::Smooth_Ion_Moments::Apply_Local_Ghost_JF");
       k_hyb_local_ghost_jf  (fa, fa->g);
       Kokkos::Profiling::popRegion();
       ism--;
@@ -152,27 +131,25 @@ hyb_advance_bpe(field_array_t * RESTRICT fa,
   // 1: Calculate electric field E_n(B_n), update B=B_n+dt/2*K1
   // ----------------------------------------------------------
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Calc_E_Update_B_K1");
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Calc_E_Update_B_K1::Advance_E");
-  hyb_advance_eue( fa, isub/nsub ); //sets ghost B + computes E
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Calc_E_Update_B_K1");
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Calc_E_Update_B_K1::Advance_E");
+  hyb_advance_ue( fa, isub/nsub ); //sets ghost B + computes E
   Kokkos::Profiling::popRegion();
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Calc_E_Update_B_K1::Remote");
-  k_begin_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );//ARI add cell-centered BCs
-  k_end_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Calc_E_Update_B_K1::Remote");
   k_begin_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   k_end_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   Kokkos::Profiling::popRegion();
 
   //fix local BCs
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Calc_E_Update_B_K1::Local");
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Calc_E_Update_B_K1::Local");
   k_hyb_local_ghost_e( fa, fa->g );
   Kokkos::Profiling::popRegion();
   
   Kokkos::MDRangePolicy<Kokkos::Rank<3>> xyz_policy({1,1,1},{nx+1,ny+1,nz+1});
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Calc_E_Update_B_K1::UpdateStencil");
-  Kokkos::parallel_for("hyb_advance_b_update1", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Calc_E_Update_B_K1::UpdateStencil");
+  Kokkos::parallel_for("hyb_advance_pe_update1", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
       INIT_STENCIL();	  
       UPDATE1();	  
     });
@@ -184,17 +161,14 @@ hyb_advance_bpe(field_array_t * RESTRICT fa,
   // 2: Update B=B_n+dt/2*K2 and store temp data
   // ----------------------------------------------------------
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Update_B_K2_Store_Temp");
-  hyb_advance_eue( fa, (isub+0.5)/nsub) ; //sets ghost B's
-  
-  k_begin_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );//ARI add cell-centered BCs
-  k_end_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );
+  Kokkos::Profiling::pushRegion("HybridAdvancePe::Update_B_K2_Store_Temp");
+  hyb_advance_ue( fa, (isub+0.5)/nsub) ; //sets ghost B's
   k_begin_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   k_end_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   //fix local BCs
   k_hyb_local_ghost_e( fa, fa->g );
   
-  Kokkos::parallel_for("hyb_advance_b_update2", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
+  Kokkos::parallel_for("hyb_advance_pe_update2", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
       INIT_STENCIL();	  
       UPDATE2();	  
     });
@@ -204,17 +178,15 @@ hyb_advance_bpe(field_array_t * RESTRICT fa,
   // 3: Update B=B_n+dt*K3 and store temp data
   // ----------------------------------------------------------
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Update_B_K3_Store_Temp");
-  hyb_advance_eue( fa, (isub+0.5)/nsub); //sets ghost B's
+  Kokkos::Profiling::pushRegion("HybyridAdvancePe::Update_B_K3_Store_Temp");
+  hyb_advance_ue( fa, (isub+0.5)/nsub); //sets ghost B's
   
-  k_begin_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );//ARI add cell-centered BCs
-  k_end_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );
   k_begin_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   k_end_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   //fix local BCs
   k_hyb_local_ghost_e( fa, fa->g );
   
-  Kokkos::parallel_for("hyb_advance_b_update3", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
+  Kokkos::parallel_for("hyb_advance_pe_update3", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
       INIT_STENCIL();	  
       UPDATE3();	  
     });
@@ -225,32 +197,18 @@ hyb_advance_bpe(field_array_t * RESTRICT fa,
   // 4: Update B=B_n+dt*(K1+2*K2+2*K3+K4)/6
   // ----------------------------------------------------------
   
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Update_B_K4");
-  hyb_advance_eue( fa, (isub+1.0)/nsub ); //sets ghost Bs
+  Kokkos::Profiling::pushRegion("HybyridAdvancePe::Update_B_K4");
+  hyb_advance_ue( fa, (isub+1.0)/nsub ); //sets ghost Bs
   
-  k_begin_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );//ARI add cell-centered BCs
-  k_end_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );
-  k_begin_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
+    k_begin_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   k_end_remote_ghost_hyb_ue( fa, fa->g, *(fa->fb) );
   //fix local BCs
   k_hyb_local_ghost_e( fa, fa->g );
   
-  Kokkos::parallel_for("hyb_advance_b_update4", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
+  Kokkos::parallel_for("hyb_advance_pe_update4", xyz_policy, KOKKOS_LAMBDA(const int x, const int y, const int z) {
       INIT_STENCIL();	  	  
       UPDATE4();	  
     });
-  Kokkos::Profiling::popRegion();
-  
-  // ----------------------------------------------------------
-  // 5: Last E update
-  // ----------------------------------------------------------
-  
-  Kokkos::Profiling::pushRegion("HybyridAdvanceB::Update_E");
-  hyb_advance_e( fa, (isub+1.0)/nsub ); //sets ghost Bs
-  k_begin_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );//ARI add cell-centered BCs
-  k_end_remote_ghost_hyb_e( fa, fa->g, *(fa->fb) );
-  //fix local BCs
-  k_hyb_local_ghost_e( fa, fa->g );
   Kokkos::Profiling::popRegion();
 
 }
