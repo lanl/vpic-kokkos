02aab787621e6fe70a143275233ac95b7f84f7f4
Nigel Phillip Tan <ntan@lanl.gov>
2025-07-02 11:27:41 -0600

    Edit sfa_private.h to remove unnecessary arguments to hyb_e and hyb_ue halo exchange.

--- src/field_advance/standard/sfa_private.h
+++ src/field_advance/standard/sfa_private.h
@@ -588,18 +588,12 @@ k_end_remote_ghost_hyb_jf( field_array_t      * ALIGNED(128) f );
 void
 k_begin_remote_ghost_hyb_e( field_array_t      * ALIGNED(128) f );
 void
-k_end_remote_ghost_hyb_e( field_array_t      * ALIGNED(128) f,
-                        const grid_t *              g,
-                        field_buffers_t& f_buffers );
+k_end_remote_ghost_hyb_e( field_array_t      * ALIGNED(128) f );
                         
 void
-k_begin_remote_ghost_hyb_ue( field_array_t      * ALIGNED(128) f,
-                            const grid_t *              g,
-                            field_buffers_t& f_buffers );
+k_begin_remote_ghost_hyb_ue( field_array_t      * ALIGNED(128) f );
 void
-k_end_remote_ghost_hyb_ue( field_array_t      * ALIGNED(128) f,
-                        const grid_t *              g,
-                        field_buffers_t& f_buffers );
+k_end_remote_ghost_hyb_ue( field_array_t      * ALIGNED(128) f );
 
 void
 k_begin_remote_ghost_hyb_curl_lpl_b( field_array_t      * ALIGNED(128) f );
