0f6ff0546da6d50984ca6e36cf69ae0dee5734b0
G. Chen <gchen@lanl.gov>
2025-07-11 16:51:23 -0600

    Add lemons_collision unit test

--- src/sf_interface/sf_interface.h
+++ src/sf_interface/sf_interface.h
@@ -225,7 +225,7 @@ typedef struct hydro_array {
   /**
     * @brief Copies the hydro data to host legacy array
     */
-  void copy_to_host(bool print= false);
+  void copy_to_host(FILE *fp=nullptr,  const int step = 0);
   // I don't think we need a copy_to_device, but I could write one easily.
 
 } hydro_array_t;
