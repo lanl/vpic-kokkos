281aea0a008722ad184425a1dd86757d4dc570e5
Ari Yitzchak Le - 280874 <arile@darwin-fe3.lanl.gov>
2025-11-05 10:13:34 -0700

    Add flags for radiation coupling

--- src/vpic/advance.cc
+++ src/vpic/advance.cc
@@ -303,7 +303,7 @@ int vpic_simulation::advance(void)
     FAK->hyb_smooth_b( field_array );
   }
   // Smooth E/B fields interpolated to particles (but not fed into B advance)
-  FAK->hyb_smooth_eb_interp( field_array);
+  FAK->hyb_smooth_eb_interp( field_array );
   KOKKOS_TOC( advance_b, 1 ); // TODO may want separate timer -ATr,2024sep10
   Kokkos::Profiling::popRegion();
 
