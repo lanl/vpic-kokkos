75494448917daebb96a7638fba3f24d3bf28a391
nphtan <nphtan2@gmail.com>
2025-12-19 09:21:26 -0700

    Fix slowdown of advance_p when using lemons collisions on GPUs. Set field member to NULL in collision operators.

--- src/collision/takizuka_abe.cc
+++ src/collision/takizuka_abe.cc
@@ -72,6 +72,7 @@ takizuka_abe(
   ta->apply_cop   = &apply_takizuka_abe_collision_op;
   ta->delete_cop  = &delete_takizuka_abe_collision_op;
   ta->next        = NULL;
+  ta->field       = NULL;
 
   REGISTER_OBJECT(ta,
                   &checkpt_takizuka_abe_collision_op,
