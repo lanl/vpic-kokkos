f01a1b170f27adec9dcfe68584e6faa90bbe8099
nphtan <nphtan2@gmail.com>
2026-06-02 15:53:42 -0600

    Fix hdf5_hydro comparison test.

--- test/integrated/hdf5_hydro/readback.py
+++ test/integrated/hdf5_hydro/readback.py
@@ -5,7 +5,7 @@ import numpy as np
 import os.path
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) > 3:
     sys.stderr.write("Usage: "+str(sys.argv[0])+" rundir\n")
     sys.exit(1)
 
@@ -15,10 +15,21 @@ nz = 1
 nxg = nx + 2
 nyg = ny + 2
 nzg = nz + 2
+num_var = 16
 
 rundir = sys.argv[1]
 
 step_names = ["0",  "1"]
+
+hydro_names = ["jx", "jy", "jz", "rho",
+               "px", "py", "pz", "rho_m",
+               "txx", "tyy", "tzz",
+               "tyz", "tzx", "txy"]
+
+if len(sys.argv) == 3 and sys.argv[2] == '--variable-charge':
+    hydro_names.append("qmin")
+    hydro_names.append("qmax")
+    num_var = 16
 
 for step_name in step_names:
     filename = rundir + "/hydro_hdf5/T." + step_name + "/hydro_ion_" + step_name + ".h5"
@@ -35,22 +46,17 @@ for step_name in step_names:
     with open(bin_filename, 'r') as fh:
         hydro_data_bi_all = np.fromfile(bin_filename, dtype=np.float32, offset=123)
 
-    hydro_names = ["jx", "jy", "jz", "rho",
-                   "px", "py", "pz", "rho_m",
-                   "txx", "tyy", "tzz",
-                   "tyz", "tzx", "txy"]
-
     for ihydro, hydro_name in enumerate(hydro_names):
         hydro_data_h5 = np.array(datagroup[hydro_name]).flatten()
-        fdata_tmp = hydro_data_bi_all[ihydro::16].reshape([nzg, nyg, nxg])
+        fdata_tmp = hydro_data_bi_all[ihydro::num_var].reshape([nzg, nyg, nxg])
         hydro_data_bi = np.ascontiguousarray((np.transpose(fdata_tmp[1:-1, 1:-1, 1:-1], axes=[2, 1, 0]))).flatten()
         
-        if np.allclose(hydro_data_bi, hydro_data_h5, atol=0.0000000001) == False:
+        if np.allclose(hydro_data_bi, hydro_data_h5, rtol=1e-5, atol=1e-10, equal_nan=True) == False:
             print(hydro_name, 'in ', 'hydro_data_h5', " does not contain same value as ", bin_filename)
             print_max_element = 0
             print("     Binary Output", ",  ", "HDF5 Output",  ",  ", "Difference")
             for i in range(0, hydro_data_h5.shape[0]):
-                if hydro_data_bi[i] - hydro_data_h5[i] > 0.0000000001:
+                if np.isclose(hydro_data_bi[i], hydro_data_h5[i], rtol=1e-5, atol=1e-10, equal_nan=True) == False:
                     print(i, hydro_data_bi[i], ",  ", hydro_data_h5[i], ",  ", hydro_data_bi[i] - hydro_data_h5[i])
                     print_max_element = print_max_element + 1
                 if print_max_element == 10:
