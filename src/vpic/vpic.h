2eeea697c4507a168a4da1905ad00e3185c57b44
nphtan <nphtan2@gmail.com>
2026-07-02 11:28:07 -0600

    Add missing charge state density flags for output.

--- src/vpic/vpic.h
+++ src/vpic/vpic.h
@@ -112,9 +112,12 @@ const uint64_t current_density  (1ULL<<0 | 1ULL<<1 | 1ULL<<2);
 const uint64_t charge_density   (1ULL<<3);
 const uint64_t momentum_density (1ULL<<4 | 1ULL<<5 | 1ULL<<6);
 const uint64_t mass_density     (1ULL<<7);
-const uint64_t stress_tensor    (1ULL<<8 | 1ULL<<9 | 1ULL<<10 | 1ULL<<11 | 1ULL<<12 | 1ULL<<13);
+const uint64_t stress_tensor    (1ULL<<8 | 1ULL<<9 | 1ULL<<10 | 
+                                 1ULL<<11 | 1ULL<<12 | 1ULL<<13);
 #ifdef VARIABLE_CHARGE
 const uint64_t charge_diags     (1ULL<<14 | 1ULL<<15);
+const uint64_t charge_state_densities (1ULL<<16 | 1ULL<<17 | 1ULL<<18 | 
+                                       1ULL<<19 | 1ULL<<20 | 1ULL<<21);
 #endif
 /* May want to use these instead
 const uint64_t stress_diagonal      (1ULL<<8 | 1ULL<<9 | 1ULL<<10);
